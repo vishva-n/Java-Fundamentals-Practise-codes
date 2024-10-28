7d4121fd3310384d1a4bdb892cd42eddfb82215c
vishva-n <vishvaanarayanasamy@gmail.com>
2024-10-28 13:33:29 +0530

    feat:new programs added

---
+++ duplicateelements.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class duplicateelements {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of an array:");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the elements for the array:");
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+    for(int i=0;i<arr.length;i++)
+    {
+        for(int j=i+1;j<arr.length;j++)
+        {
+            if (arr[i]==arr[j])
+            {
+                System.out.print(arr[j]+ " ");
+            }
+
+        }
+    }
+    }
+}
---
+++ reverseanarray.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class reverseanarray {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array: ");
+        int size=sc.nextInt();
+        System.out.println("Enter the values for the matrix:");
+        int arr[]=new int[size];
+        for(int i=0;i<arr.length;i++)
+        {
+           arr[i]=sc.nextInt();
+        }
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+}
